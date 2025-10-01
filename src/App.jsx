d362ec0e2d17faf1f90a524b474ecbf5babc3e8e
Paulo Quispe <paulo.quispe.horna@gmail.com>
2025-10-01 01:54:09 -0500

    Trabajando con mocks, estado y promesas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from "react";
+import jokes from "./mocks/jokes.json";
+
+const getMockJoke = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(jokes);
+    }, 2000);
+  });
+};
+
 export default function App() {
+  const [joke, setJoke] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const getRandomJoke = async () => {
+    setJoke(null);
+    setLoading(true);
+    try {
+      const data = await getMockJoke();
+      setJoke(data.joke);
+      const largeWord = data.joke
+        .split(" ")
+        .reduce((previousWord, currentWord) =>
+          currentWord.length > previousWord.length ? currentWord : previousWord
+        );
+      console.log(largeWord);
+    } catch (error) {
+      setJoke(`Error: ${error}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getRandomJoke();
+  }, []);
+
   return (
-    <h1>Hello, World!</h1>
-  )
+    <>
+      <h1>Random Jokes App</h1>
+      <button onClick={getRandomJoke} disabled={loading}>
+        {loading ? "Loading..." : "Get Joke"}
+      </button>
+      {joke ? <p>{joke}</p> : <p>Please wait a moment...</p>}
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import App from "./App"
-import "./style.css"
+import App from "./App";
+import "./style.css";
 
-const root = createRoot(document.getElementById("app"))
+const root = createRoot(document.getElementById("app"));
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
